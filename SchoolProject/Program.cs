1ccceb3adc255db2708eca530a719e1d558b5159
AhmedHazem02 <ahemd01068@gmail.com>
2024-11-15 16:00:11 +0200

    Update

--- SchoolProject/Program.cs
+++ SchoolProject/Program.cs
@@ -13,58 +13,58 @@ using SchoolProject.API.Extention;
 using SchoolProject.infransturture.DataSeeding;
 using System.Data;
 
-var builder = WebApplication.CreateBuilder(args);
+            var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
+            builder.Services.AddControllers();
+      
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
 
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+            #region DataBase
+            builder.Services.AddDbContext<AppDbContext>(option =>
+            {
+                option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+            });
 
-#region DataBase
-builder.Services.AddDbContext<AppDbContext>(option =>
-{
-    option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
+            builder.Services.AddDbContext<AppIdentityDbContext>(option =>
+            {
+                option.UseSqlServer(builder.Configuration.GetConnectionString("IdentityConnection"));
+            });
+            #endregion
+            #region Dependecy Injection
+            builder.Services.AddModuleInfranstructure_ID()
+                                  .Addservices_DI()
+                                  .AddCore_DI()
+                                  .AddServiceRegisteration(builder.Configuration);
+            #endregion
 
-builder.Services.AddDbContext<AppIdentityDbContext>(option =>
-{
-    option.UseSqlServer(builder.Configuration.GetConnectionString("IdentityConnection"));
-});
-#endregion
-#region Dependecy Injection
-builder.Services.AddModuleInfranstructure_ID()
-                      .Addservices_DI()
-                      .AddCore_DI()
-                      .AddServiceRegisteration(builder.Configuration);
-#endregion
+            var app = builder.Build();
 
-var app = builder.Build();
+            #region Dataseeding
+            using (var scope = app.Services.CreateScope())
+            {
+                var userManager = scope.ServiceProvider.GetRequiredService<UserManager<AppUser>>();
+                var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+                await RoleSeeder.SeedAsync(roleManager);
+                await UserSeeder.SeedAsync(userManager);
+            }
 
-#region Dataseeding
-using (var scope = app.Services.CreateScope())
-{
-    var userManager = scope.ServiceProvider.GetRequiredService<UserManager<AppUser>>();
-    var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
-    await RoleSeeder.SeedAsync(roleManager);
-    await UserSeeder.SeedAsync(userManager);
-}
-
-#endregion
-
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-app.UseMiddleware<ErrorHandlerMiddleware>();
-app.UseHttpsRedirection();
-
-app.UseAuthentication();
-app.UseAuthorization();
+            #endregion
 
 
-app.MapControllers();
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+            app.UseMiddleware<ErrorHandlerMiddleware>();
+            app.UseHttpsRedirection();
+             
+            app.UseAuthentication();
+            app.UseAuthorization();
 
-app.Run();
+
+            app.MapControllers();
+
+            app.Run();
